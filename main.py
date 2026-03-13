bb62e03179f11657887c0d909ad0587172fc5b8b
khairiansyah23 <khairiansyahf@gmail.com>
2026-03-13 15:28:54 +0700

    Update Book instances and add title methods

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from book import Book
 
-b1 = Book(1, "Learn Python", "Affan")
-b2 = Book(2, "Mastering OOP", "Budi")
+b1 = Book(1122, "Learn Python", "pasep")
+b2 = Book(1001, "Unit Test", "skyla")
 
-print(b1.title)
-print(b2.title)
+print(b1.get_title())
+print(b2.get_title())
+
+b1.set_title("Belajar Python")
+
+print(b1.get_title())
+print(b2.get_title())
