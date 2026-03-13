5928ce53ccf5de081d6a730dff0d9ac0a6644da9
khairiansyah23 <khairiansyahf@gmail.com>
2026-03-13 15:28:16 +0700

    Add getter and setter methods for title and id

--- book.py
+++ book.py
@@ -6,3 +6,12 @@ class Book:
         self.title = title
         self.author = author
         self.last_updated = datetime.now()
+
+def get_id(self):
+        return self.__id
+
+    def get_title(self):
+        return self.__title
+
+    def set_title(self, new_title):
+        self.__title = new_title
